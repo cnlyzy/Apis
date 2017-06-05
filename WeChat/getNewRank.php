3eaa582b4363bdb5bbc85f91a732fd6bccdaa67d
陈优雅 <cnlyzy@qq.com>
2017-06-05 21:58:34 +0800

    add newrank

---
+++ WeChat/getNewRank.php
@@ -0,0 +1,59 @@
+<?php
+
+//init
+$yesterday = date("Y-m-d", strtotime("1 days ago"));
+$start = $yesterday;
+$end = $yesterday;
+$nonce = '012345678';
+$appBase = '/xdnphb';
+$urlBase = $appBase . '/';
+$xyz_str = $urlBase . 'list/day/rank?AppKey=joker&end=' . $end . '&rank_name=时事&rank_name_group=资讯&start=' . $start;
+$xyz = md5($xyz_str . '&nonce=' . $nonce);
+//print_r($xyz);
+
+$postData = [
+    'end' => $end,
+    'rank_name' => '时事',
+    'rank_name_group' => '资讯',
+    'start' => $start,
+    'nonce' => $nonce,
+    'xyz' => $xyz
+];
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://www.newrank.cn/xdnphb/list/day/rank",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => http_build_query($postData),
+    CURLOPT_HTTPHEADER => array(
+        'Host: www.newrank.cn',
+        'Connection: keep-alive',
+        'Accept: application/json, text/javascript, */*; q=0.01',
+        'Origin: http://www.newrank.cn',
+        'X-Requested-With: XMLHttpRequest',
+        'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+        'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+        'DNT: 1',
+        'Referer: http://www.newrank.cn/public/info/hot.html?period=day',
+        'Accept-Encoding: gzip, deflate',
+        'Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4'
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
